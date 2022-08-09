f829266f0c7d1095b68aee76d102c6105446c274
Kevin2133 <68924818+Kevin2133@users.noreply.github.com>
2022-08-09 13:17:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const url = "https://easycodingkevapi.herokuapp.com/";
 const div = document.getElementById("apiRes");
+const lnbr = document.createElement("br");
+var par = document.createElement("p");
 
 window.onload = () => {
     fetch(url)
@@ -8,5 +10,13 @@ window.onload = () => {
     })
     .then((data) => {
         console.log(data);
+        par.innerHTML = data.name;
+        div.appendChild(par);
+        div.appendChild(lnbr);
+        par.innerHTML = data.surname;
+        div.appendChild(par);
+        div.appendChild(lnbr);
+        par.innerHTML = data.level;
+        div.appendChild(par);
     });
 };
