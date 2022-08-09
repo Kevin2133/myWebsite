7cd1b138da8d55f154549429612bff054b2b74f6
Kevin2133 <kevin.bellocchio@gmail.com>
2022-08-09 13:21:37 +0200

    ok

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const url = "https://easycodingkevapi.herokuapp.com/";
 const div = document.getElementById("apiRes");
 const lnbr = document.createElement("br");
-var par = document.createElement("p");
 
 window.onload = () => {
     fetch(url)
@@ -9,6 +8,7 @@ window.onload = () => {
         return res.json();
     })
     .then((data) => {
+        var par = document.createElement("p");
         console.log(data);
         par.innerHTML = data.name;
         div.appendChild(par);
